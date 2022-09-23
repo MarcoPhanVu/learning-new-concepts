316fbbe4dc800430adbd8cc64975a722784db1fd
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-09-24 00:15:56 +0700

    remove excess logs and reduce code length

--- script.js
+++ script.js
@@ -23,13 +23,9 @@ let checkPalindrome = text => isPalindrome(text) ? `"${text}" is a Palindrome.` 
 resultsContainer[0].innerHTML = checkPalindrome(chosenTextPalindrome);
 
 function isPalindrome(text) {
-    if (text.length <= 1) {
-        return true;
-    }
+    if (text.length <= 1) return true;
 
-    if (text[0] == text[text.length - 1]) {
-        return isPalindrome(text.substring(1, text.length - 1));
-    }
+    if (text[0] == text[text.length - 1]) return isPalindrome(text.substring(1, text.length - 1));
 
     return false;
 }
@@ -45,63 +41,45 @@ let pickedNum = 0;
 for (let i = 0; i < range; ++i) { //for better looking result
     pickedNum = randomRange(0, 50);
     numbList.push(pickedNum);
-    if (i != range - 1) {
-        dataValues[1].innerHTML += pickedNum + ", ";
-    } else {
-        dataValues[1].innerHTML += pickedNum;
-    }
+
+    if (i != range - 1) dataValues[1].innerHTML += pickedNum + ", ";
+    
+    else dataValues[1].innerHTML += pickedNum;
 }
+
 
 
 mergeSort(numbList, 0, numbList.length);
 
+
+
 for (let i = 0; i < numbList.length; ++i) { //for better looking result
-    if (i != numbList.length - 1) {
-        resultsContainer[1].innerHTML += numbList[i] + ", ";
-    } else {
-        resultsContainer[1].innerHTML += numbList[i];
-    }
+    if (i != numbList.length - 1) resultsContainer[1].innerHTML += numbList[i] + ", ";
+    else resultsContainer[1].innerHTML += numbList[i];
 }
 
 function mergeArray(arr, leftPointer, middlePoint, rightPointer) {
     let tempArray = [];
-    for (let i = 0; i < arr.length; ++i) {
-        tempArray.push(0);
-    }
-    console.log("length: " + arr.length)
-    console.log("tempArr = " + tempArray)
+    for (let i = 0; i < arr.length; ++i) tempArray.push(0);
 
     let leftCount = leftPointer;
     let midCount = middlePoint + 1;
     let indexCount = leftPointer;
 
     while (leftCount <= middlePoint && midCount <= rightPointer) {
-        console.log("Shit went here")
-        if (arr[leftCount] > arr[midCount]) {
-            tempArray[indexCount++] = arr[midCount++];
-        } else {
-            tempArray[indexCount++] = arr[leftCount++];
-        }
+        if (arr[leftCount] > arr[midCount]) tempArray[indexCount++] = arr[midCount++];
+
+        else tempArray[indexCount++] = arr[leftCount++];
     }
 
-    while (leftCount <= middlePoint) {
-        tempArray[indexCount++] = arr[leftCount++];
-    }
+    while (leftCount <= middlePoint) tempArray[indexCount++] = arr[leftCount++];
 
-    while (midCount <= rightPointer) {
-        tempArray[indexCount++] = arr[midCount++];
-    }
+    while (midCount <= rightPointer) tempArray[indexCount++] = arr[midCount++];
 
-    if(indexCount > arr.length) { //Prevent creating fake elements
-        indexCount = arr.length;
-    }
+    if(indexCount > arr.length) indexCount = arr.length; //Prevent creating fake elements
 
-    for (let i = leftPointer; i < indexCount; ++i) {
-        arr[i] = tempArray[i];
-    }
+    for (let i = leftPointer; i < indexCount; ++i) arr[i] = tempArray[i];
 }
-
-
 
 function mergeSort(arr, leftPointer, rightPointer) {
     if (leftPointer < rightPointer) {
@@ -116,13 +94,6 @@ function mergeSort(arr, leftPointer, rightPointer) {
 
     return;
 }
-
-    // New problems:
-    //     - At the end of the loop, sometimes, the array will have an extra element due to {indexCount} being incremented by 1. Solution?
-    //         + Force it to be equal to arr.length  -> testing
-    //         + For loop will be set to arr.length
-    //     - Elements are being duplicated through out the sorting process
-    //         + Dunno yet
 
 //2ND SECTION END
 
