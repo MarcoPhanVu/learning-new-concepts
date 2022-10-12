2737e988d7ac52dc96a749b36a301378818a1d27
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-10-13 01:08:11 +0700

    add classes.js

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,13 +1,22 @@
 class Rectangle {
 
-    constructor(width, height, color) {
-        this.widht = width;
-        this.height = height;
-        this.color = color;
+    constructor(_width, _height, _color = "Teal") {
+        this.width = _width;
+        this.height = _height;
+        this.color = _color;
+    }
+
+    get area() { // Syntax: shape.area
+        return this.width * this.height;
+    }
+
+    static swapWidth(rectA, rectB) { //For the Class it self, syntax: Rectangle.swapWidth(shape0, shape1);
+        let temp = rectA.width;
+        rectA.width = rectB.width;
+        rectB.width = temp;
     }
 
     greeting() {
-        console.log(this.width * this.height)
         console.log(`Hello, my name is ${this.color}${this.width * this.height}.`);
     }
 
@@ -16,8 +25,13 @@ class Rectangle {
     }
 }
 
-
-const shape0 = new Rectangle(4, 6, "Green");
+const shape0 = new Rectangle(40, 6, "Green");
+const shape1 = new Rectangle(4, 55)
 
 shape0.greeting();
 shape0.talkshit();
+shape0.area;
+
+Rectangle.swapWidth(shape0, shape1);
+console.log(shape0);
+console.log(shape1);
