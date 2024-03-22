017b6a577b0c18859a12fa5d7f84f7af36028db1
Marco <phanvutuananh75@gmail.com>
2024-03-22 16:22:34 +0700

    late reformat for better visuability

--- scripts/recursion.js
+++ scripts/recursion.js
@@ -12,9 +12,13 @@
     resultsContainer[0].innerHTML = checkPalindrome(chosenTextPalindrome);
     
     function isPalindrome(text) {
-        if (text.length <= 1) return true;
+        if (text.length <= 1) {
+            return true;
+        }
     
-        if (text[0] == text[text.length - 1]) return isPalindrome(text.substring(1, text.length - 1));
+        if (text[0] == text[text.length - 1]) {
+            return isPalindrome(text.substring(1, text.length - 1));
+        }
     
         return false;
     }
@@ -31,39 +35,56 @@
         pickedNum = randomRange(0, 30);
         numbList.push(pickedNum);
     
-        if (i != range - 1) dataValues[1].innerHTML += pickedNum + ", ";
-        
-        else dataValues[1].innerHTML += pickedNum;
+        if (i != range - 1) {
+            dataValues[1].innerHTML += pickedNum + ", ";
+        } else {
+            dataValues[1].innerHTML += pickedNum;
+        }
     }
     
     mergeSort(numbList, 0, numbList.length);
     
     for (let i = 0; i < numbList.length; ++i) { //for better looking result
-        if (i != numbList.length - 1) resultsContainer[1].innerHTML += numbList[i] + ", ";
-        else resultsContainer[1].innerHTML += numbList[i];
+        if (i != numbList.length - 1) {
+            resultsContainer[1].innerHTML += numbList[i] + ", ";
+        } else {
+            resultsContainer[1].innerHTML += numbList[i];
+        }
     }
     
     function mergeArray(arr, leftPointer, middlePoint, rightPointer) {
         let tempArray = [];
-        for (let i = 0; i < arr.length; ++i) tempArray.push(0);
+        for (let i = 0; i < arr.length; ++i) {
+            tempArray.push(0);
+        }
     
         let leftCount = leftPointer;
         let midCount = middlePoint + 1;
         let indexCount = leftPointer;
     
         while (leftCount <= middlePoint && midCount <= rightPointer) {
-            if (arr[leftCount] > arr[midCount]) tempArray[indexCount++] = arr[midCount++];
-    
-            else tempArray[indexCount++] = arr[leftCount++];
+            if (arr[leftCount] > arr[midCount]) {
+                tempArray[indexCount++] = arr[midCount++];
+            } else {
+                tempArray[indexCount++] = arr[leftCount++];
+            }
         }
     
-        while (leftCount <= middlePoint) tempArray[indexCount++] = arr[leftCount++];
+        while (leftCount <= middlePoint) {
+            tempArray[indexCount++] = arr[leftCount++];
+        }
     
-        while (midCount <= rightPointer) tempArray[indexCount++] = arr[midCount++];
+        while (midCount <= rightPointer) {
+            tempArray[indexCount++] = arr[midCount++];
+        }
     
-        if (indexCount > arr.length) indexCount = arr.length; //Prevent creating fake elements
+        if (indexCount > arr.length) {
+            indexCount = arr.length; //Prevent creating fake elements
+        }
     
-        for (let i = leftPointer; i < indexCount; ++i) arr[i] = tempArray[i];
+        for (let i = leftPointer; i < indexCount; ++i) {
+            arr[i] = tempArray[i];
+        }
     }
     
     function mergeSort(arr, leftPointer, rightPointer) {
